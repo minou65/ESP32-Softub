a63c8a383fc7f36b920f23093387af611342a043
Andy <azogg65@bluewin.ch>
2024-03-17 11:44:25 +0100

    convert functions added

--- src/common.cpp
+++ src/common.cpp
@@ -1,7 +1,16 @@
-// 
-// 
-// 
-
 #include "common.h"
 
+double fahrenheitToCelsius(double fahrenheit) {
+    double celsius;
 
+    celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+    return celsius;
+}
+
+
+double celsiusToFahrenheit(double celsius) {
+	double fahrenheit;
+
+	fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+	return fahrenheit;
+}
