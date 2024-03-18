6c1ba063f74b1a2b86adfd5ca5283b492f070419
Andreas Zogg <andreas.zogg@swisscom.com>
2024-03-18 07:35:16 +0100

    pins for panel changed

--- src/pins.h
+++ src/pins.h
@@ -16,11 +16,8 @@ const int pin_temp[] = { GPIO_NUM_32, GPIO_NUM_33 };
 #define pin_green GPIO_NUM_13
 #define pin_blue GPIO_NUM_14
 
-// Make sure the pins shared with hardware SPI via the shield are high-impedance.
-// The spot for pin 13 is defined as "no connection" on this one
-
-#define PANEL_TX GPIO_NUM_4
-#define PANEL_RX GPIO_NUM_5
+#define PANEL_TX GPIO_NUM_8
+#define PANEL_RX GPIO_NUM_7
 
 
 #define TSENS_OFFSET (-113)
